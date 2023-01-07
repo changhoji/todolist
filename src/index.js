d3ec2d7d5eeff5faa95a188d18fa6736a1026a8c
changhoji <onkkno1@gmail.com>
2023-01-08 02:02:18 +0900

    feat: connect to firebase

--- src/index.js
+++ src/index.js
@@ -2,5 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
+import firebase from "fbApp";
+
+console.log(firebase);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
